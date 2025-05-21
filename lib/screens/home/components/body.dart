f0a6856c6a071c80b1f6866206ece991afdec485
defarrel <Defarrel>
2025-05-21 15:40:26 +0700

    Membuat text recommended dengan underline

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -13,9 +13,43 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
+          TitleWithCustomUnderLine(text: "Recommended"),
         ],
       ),
     );
   }
 }
 
+class TitleWithCustomUnderLine extends StatelessWidget {
+  const TitleWithCustomUnderLine({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
