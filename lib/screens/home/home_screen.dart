ffd858a67eb7a421dc2eebc01c00e80e0fbc8887
defarrel <Defarrel>
2025-05-21 17:20:57 +0700

    Membuat bottomnavbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plantapp/components/bottom_nav_bar.dart';
 import 'package:plantapp/constant.dart';
 import 'package:plantapp/screens/home/components/body.dart';
 
@@ -7,11 +8,11 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
+      bottomNavigationBar: BottomNavBar(),
     );
   }
 
