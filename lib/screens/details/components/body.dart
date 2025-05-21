7a59b59e5b72a906f3a6adc2b9a6e34fc4ac91cc
defarrel <Defarrel>
2025-05-21 18:05:22 +0700

    Memisahkan image icon dan icon card dari body

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constant.dart';
+import 'package:plantapp/screens/details/components/icon_card.dart';
+import 'package:plantapp/screens/details/components/image_and_icons.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -8,100 +10,63 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        SizedBox(
-          height: size.height * 0.8,
-          child: Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: kDefaultPadding * 3,
-                  ),
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: IconButton(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: kDefaultPadding,
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
-                        ),
-                      ),
-                      Spacer(),
-                      IconCard(icon: "assets/icons/sun.svg"),
-                      IconCard(icon: "assets/icons/icon_2.svg"),
-                      IconCard(icon: "assets/icons/icon_3.svg"),
-                      IconCard(icon: "assets/icons/icon_4.svg"),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: size.height * 0.8,
-                width: size.width * 0.75,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(63),
-                    bottomLeft: Radius.circular(63),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(0, 10),
-                      blurRadius: 60,
-                      color: kPrimaryColor.withOpacity(0.35),
-                    ),
-                  ],
-                  image: DecorationImage(
-                    alignment: Alignment.topLeft,
-                    fit: BoxFit.cover,
-                    image: AssetImage("assets/images/img.png"),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ImageAndIcons(size: size),
+          TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+        ],
+      ),
     );
   }
 }
 
-class IconCard extends StatelessWidget {
-  const IconCard({super.key, required this.icon});
+class TitleAndPrice extends StatelessWidget {
+  const TitleAndPrice({
+    super.key,
+    required this.title,
+    required this.country,
+    required this.price,
+  });
 
-  final String icon;
+  final String title, country;
+  final int price;
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
-      padding: EdgeInsets.all(kDefaultPadding / 2),
-      height: 62,
-      width: 62,
-      decoration: BoxDecoration(
-        color: kBackgroundColor,
-        borderRadius: BorderRadius.circular(6),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 15),
-            blurRadius: 22,
-            color: kPrimaryColor.withOpacity(0.22),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: [
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "$title\n",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.headlineMedium?.copyWith(color: kTextColor),
+                ),
+                TextSpan(
+                  text: "$country",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
           ),
-          BoxShadow(
-            offset: Offset(-15, -15),
-            blurRadius: 20,
-            color: Colors.white,
+          Spacer(),
+          Text(
+            "\$$price",
+            style: Theme.of(
+              context,
+            ).textTheme.headlineSmall?.copyWith(color: kPrimaryColor),
           ),
         ],
       ),
-      child: SvgPicture.asset(icon),
     );
   }
 }
