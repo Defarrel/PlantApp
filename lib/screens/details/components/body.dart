f48846ce6bcfbb2ec856e1accde5af6b52369547
defarrel <Defarrel>
2025-05-21 18:06:43 +0700

    Menambahkan title and pirce dan memisahkannya dari body

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constant.dart';
 import 'package:plantapp/screens/details/components/icon_card.dart';
 import 'package:plantapp/screens/details/components/image_and_icons.dart';
+import 'package:plantapp/screens/details/components/title_and_price.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -15,56 +16,6 @@ class Body extends StatelessWidget {
         children: [
           ImageAndIcons(size: size),
           TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
-        ],
-      ),
-    );
-  }
-}
-
-class TitleAndPrice extends StatelessWidget {
-  const TitleAndPrice({
-    super.key,
-    required this.title,
-    required this.country,
-    required this.price,
-  });
-
-  final String title, country;
-  final int price;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Row(
-        children: [
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: "$title\n",
-                  style: Theme.of(
-                    context,
-                  ).textTheme.headlineMedium?.copyWith(color: kTextColor),
-                ),
-                TextSpan(
-                  text: "$country",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: kPrimaryColor,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Spacer(),
-          Text(
-            "\$$price",
-            style: Theme.of(
-              context,
-            ).textTheme.headlineSmall?.copyWith(color: kPrimaryColor),
-          ),
         ],
       ),
     );
