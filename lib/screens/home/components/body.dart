5832e89ad467fd9a6927dcb2c4e4f8d5bb6f8733
defarrel <Defarrel>
2025-05-21 15:58:28 +0700

    Memisahkan file untuk button more

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constant.dart';
 import 'package:plantapp/screens/home/components/header_with_searchbox.dart';
+import 'package:plantapp/screens/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -14,71 +15,6 @@ class Body extends StatelessWidget {
         children: <Widget>[
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: "Recommended", press: () {}),
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithMoreBtn extends StatelessWidget {
-  const TitleWithMoreBtn({super.key, required this.title, required this.press});
-
-  final String title;
-  final VoidCallback press;
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Row(
-        children: [
-          TitleWithCustomUnderLine(text: title),
-          Spacer(),
-          TextButton(
-            style: TextButton.styleFrom(
-              backgroundColor: kPrimaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-            onPressed: press,
-            child: Text("More", style: TextStyle(color: Colors.white)),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithCustomUnderLine extends StatelessWidget {
-  const TitleWithCustomUnderLine({super.key, required this.text});
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-            child: Text(
-              text,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              margin: EdgeInsets.only(right: kDefaultPadding / 4),
-              height: 7,
-              color: kPrimaryColor.withOpacity(0.2),
-            ),
-          ),
         ],
       ),
     );
