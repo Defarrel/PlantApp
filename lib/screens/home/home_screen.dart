44b39b7e2b225aee10b22db181094278ff199049
defarrel <Defarrel>
2025-05-21 15:00:31 +0700

    Menambahkan body dan textfield seacrh

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constant.dart';
+import 'package:plantapp/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
+      body: Body(),
     );
   }
 
