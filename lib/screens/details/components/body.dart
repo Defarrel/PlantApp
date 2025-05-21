be4db3546d5848e089f657a5cb1cb62b65c5a1e3
defarrel <Defarrel>
2025-05-21 17:44:36 +0700

    Menambahkan arrow back

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constant.dart';
 
 class Body extends StatelessWidget {
@@ -9,32 +10,58 @@ class Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Column(
       children: [
-        Row(
-          children: [
-            Expanded(child: Column()),
-            Container(
-              height: size.height * 0.8,
-              width: size.width * 0.75,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 60,
-                    color: kPrimaryColor.withOpacity(0.35),
-                  )
-                ],
-                image: DecorationImage(
-                  alignment: Alignment.topLeft,
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/images/img.png"),
+        SizedBox(
+          height: size.height * 0.8,
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: kDefaultPadding * 3,
+                  ),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: kDefaultPadding,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Container(
+                height: size.height * 0.8,
+                width: size.width * 0.75,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(63),
+                    bottomLeft: Radius.circular(63),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 60,
+                      color: kPrimaryColor.withOpacity(0.35),
+                    ),
+                  ],
+                  image: DecorationImage(
+                    alignment: Alignment.topLeft,
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/img.png"),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
