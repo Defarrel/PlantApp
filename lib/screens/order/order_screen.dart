373c0f355687304cd26b79afc7847f36cd617597
defarrel <Defarrel>
2025-05-22 12:00:43 +0700

    Menambahkan file order screen dan body untuk order page

---
+++ lib/screens/order/order_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/screens/order/components/body.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
